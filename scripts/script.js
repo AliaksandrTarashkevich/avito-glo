29ae03d83e0a80fd20d03f6d7a70ad4680ed5aea
Alex Tarashkevich <tal@db.by>
2020-07-08 21:12:33 +0300

    js in progress

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+alert('Hello World!');
+
+function foo() {
+  console.log("FD");
+}
+
+const func = function() {
+  console.log("FE");
+};
+
+const arrow = () => {
+  console.log("Arrow Function");
+};
+foo();
+func();
+arrow();
